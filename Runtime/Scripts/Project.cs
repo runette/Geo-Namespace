7bf0a160eb87ec3cbfeca3ec3a257c7954d813ac
runette <runette@gmail.com>
2023-02-10 16:08:46 +0000

    Basic Prototyps created in Basic Types

--- Runtime/Scripts/Project.cs
+++ Runtime/Scripts/Project.cs
@@ -1,66 +1,42 @@
-﻿
-
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.IO;
 using System;
-using GeoJSON.Net.Geometry;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json.Converters;
 using System.ComponentModel;
-using UnityEngine;
-
+using Virgis;
+using System.Linq;
 
 namespace Project
 {
-    public class GisProject : TestableObject
+    public class GisProject : GisProjectPrototype
     {
-
-        private const string TYPE = "geo";
-        private const string VERSION = "1.0.5";
-
         [JsonIgnore]
-        public string path { set
+        public string path
+        {
+            set
             {
                 foreach (RecordSet set in RecordSets)
                 {
                     set.path = value;
                 }
-            } }
+            }
+        }
+        private const string TYPE = "geo";
+        private const string VERSION = "1.1.0";
 
         public static string GetVersion()
         {
             return $"{TYPE}:{VERSION}";
         }
 
-        [JsonProperty(PropertyName = "version", Required = Required.Always)]
-        public string ProjectVersion;
-
-        [JsonProperty(PropertyName = "name", Required = Required.Always)]
-        public string Name;
-
-        [JsonProperty(PropertyName = "origin", Required = Required.Always)]
-        public Point Origin;
-
-        [JsonProperty(PropertyName = "scales")]
-        public List<float> Scale;
-
-        [JsonProperty(PropertyName = "default_proj")]
-        public string projectCrs;
-
-        [JsonProperty(PropertyName = "grid-scale")]
-        public float GridScale;
-
-        [JsonProperty(PropertyName = "cameras", Required = Required.Always)]
-        public List<Point> Cameras;
-
         [JsonProperty(PropertyName = "recordsets", Required = Required.Always)]
         [JsonConverter(typeof(RecordsetConverter))]
         public List<RecordSet> RecordSets;
     }
 
-    public class RecordSet : TestableObject
+    public class RecordSet : RecordSetPrototype
     {
         [JsonIgnore]
         private string m_Path;
@@ -72,86 +48,20 @@ namespace Project
                 Properties.path = value;
             } }
 
-        [JsonProperty(PropertyName = "id", Required = Required.Always)]
-        public string Id;
-        [JsonProperty(PropertyName = "display-name")]
-        public string DisplayName;
         [JsonProperty(PropertyName = "datatype", Required = Required.Always)]
         [JsonConverter(typeof(StringEnumConverter))]
         public RecordSetDataType DataType;
         [JsonProperty(PropertyName = "source")]
         public string m_source;
-
         [JsonIgnore]
         public string Source { get { return Path.GetFullPath(Path.Combine( path, m_source)); }
             set { m_source = value; } }
-        [JsonProperty(PropertyName = "position")]
-        public Point Position;
-        [JsonProperty(PropertyName = "transform")]
-        public JsonTransform Transform;
         [JsonProperty(PropertyName = "properties")]
         public GeogData Properties;
-        [JsonProperty(PropertyName = "visible", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
-        [DefaultValue(true)]
-        public bool Visible;
         [JsonProperty(PropertyName = "proj4")]
         public string Crs;
     }
 
-    public class JsonTransform : TestableObject
-    {
-        [JsonProperty(PropertyName = "translate", Required = Required.Always)]
-        [JsonConverter(typeof(VectorConverter<SerializableVector3>))]
-        public SerializableVector3 Position;
-        [JsonProperty(PropertyName = "rotate", Required = Required.Always)]
-        [JsonConverter(typeof(VectorConverter<SerializableQuaternion>))]
-        public SerializableQuaternion Rotate;
-        [JsonProperty(PropertyName = "scale", Required = Required.Always)]
-        [JsonConverter(typeof(VectorConverter<SerializableVector3>))]
-        public SerializableVector3 Scale;
-
-        public static JsonTransform zero()
-        {
-            return new JsonTransform() { Position = Vector3.zero, Rotate = Quaternion.identity, Scale = Vector3.zero };
-        }
-    }
-
-    public class VectorConverter<T> : JsonConverter where T : Serializable, new()
-    {
-        public VectorConverter()
-        {
-
-        }
-
-        public override bool CanConvert(Type objectType)
-        {
-            return typeof(T).IsAssignableFrom(objectType);
-        }
-
-        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-        {
-            switch (reader.TokenType)
-            {
-                case JsonToken.Null:
-                    return null;
-                case JsonToken.StartArray:
-                    JArray jarray = JArray.Load(reader);
-                    IList<float> values = jarray.Select(c => (float)c).ToList();
-                    T result = new T();
-                    result.Update(values);
-                    return result;
-            }
-
-            throw new JsonReaderException("expected null, object or array token but received " + reader.TokenType);
-        }
-
-
-        public override void WriteJson(JsonWriter writer, object vector, JsonSerializer serializer)
-        {
-            T newvector = (T)vector;
-            serializer.Serialize(writer, newvector.ToArray());
-        }
-    }
 
     public class RecordsetConverter : JsonConverter
     {
@@ -175,7 +85,8 @@ namespace Project
                     JArray jarray = JArray.Load(reader);
                     IList<JObject> sets = jarray.Select(c => (JObject)c).ToList();
                     List<RecordSet> result = new List<RecordSet>();
-                    foreach (JObject set in sets) {
+                    foreach (JObject set in sets)
+                    {
                         result.Add(set.ToObject(typeof(RecordSet)) as RecordSet);
                     }
                     return result;
@@ -454,18 +365,12 @@ namespace Project
         TCP,
     }
 
-    public class Unit : TestableObject
+    /// <summary>
+
+    public class Unit : UnitPrototype
     {
         [JsonIgnore]
         public string path;
-        /// <summary>
-        /// Color used for the unit of symbology.
-        /// 
-        /// Can be in either integer[0 .. 255] format or float[0..1] format
-        /// </summary>
-        [JsonProperty(PropertyName = "color", Required = Required.Always)]
-        [JsonConverter(typeof(VectorConverter<SerializableColor>))]
-        public SerializableColor Color;
         /// <summary>
         /// The shape to be used by the unit of symbology.
         /// 
@@ -477,13 +382,6 @@ namespace Project
         /// <summary>
         /// The transfor to be applied to the unit of symnbology
         /// </summary>
-        [JsonProperty(PropertyName = "transform", Required = Required.Always)]
-        public JsonTransform Transform;
-        /// <summary>
-        /// The name of a field in the metadata to be used a label for the data entity
-        /// </summary>
-        [JsonProperty(PropertyName = "label")]
-        public string Label;
 
         [JsonProperty(PropertyName = "texture-image")]
         public string m_TextureImage;
@@ -491,46 +389,17 @@ namespace Project
         [JsonIgnore]
         public string TextureImage
         {
-            get { if (m_TextureImage is not null && m_TextureImage != "")
+            get
+            {
+                if (m_TextureImage is not null && m_TextureImage != "")
                 {
                     return Path.GetFullPath(Path.Combine(path, m_TextureImage));
                 }
-                else {
+                else
+                {
                     return null;
                 }
             }
         }
     }
-
-    /// <summary>
-    /// Acceptable value for the Shape field in Symbology
-    /// </summary>
-    public enum Shapes
-    {
-        Spheroid,
-        Cuboid,
-        Cylinder
-    }
-
-    /// <summary>
-    /// Acceptable values for color-mode
-    /// </summary>
-    public enum ColorMode
-    {
-        MultibandColor,
-        SinglebandColor,
-        SinglebandGrey
-    }
-
-    /// <summary>
-    /// Generic class to make an entity testabble - to allow the members to be tested for their presence
-    /// </summary>
-    public class TestableObject
-    {
-        public bool ContainsKey(string propName)
-        {
-            return GetType().GetMember(propName) != null;
-        }
-    }
-
 }
